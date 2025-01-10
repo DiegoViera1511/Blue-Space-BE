fe0ab44960a96e9c535ef16868b0d81cbe9699df
Diego Viera <diegovieramartinez@gmail.com>
2025-01-10 11:26:03 -0500

    added token sesion to user schema

--- src/features/User/schemas.ts
+++ src/features/User/schemas.ts
@@ -3,6 +3,7 @@ import {pgTable, varchar} from 'drizzle-orm/pg-core';
 export const user = pgTable('user', {
     username: varchar('username', { length: 255 }).primaryKey().notNull(),
     password: varchar('password', { length: 255 }).notNull(),
+    token: varchar('token')
 });
 
 export type User = typeof user.$inferSelect;
