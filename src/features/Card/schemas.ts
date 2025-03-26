18ce42be3c98321aaee396d7aa06ad3aa32fd2fc
Diego Viera <diegovieramartinez@gmail.com>
2025-03-25 23:03:14 -0400

    added participant to cards

--- src/features/Card/schemas.ts
+++ src/features/Card/schemas.ts
@@ -1,6 +1,7 @@
 import {integer, pgTable, varchar} from 'drizzle-orm/pg-core';
 import {uuid} from "drizzle-orm/pg-core/columns/uuid";
 import {state} from "../State/schemas";
+import {user} from "../User/schemas";
 
 export const card = pgTable('card', {
     id: uuid('id').primaryKey().defaultRandom(),
@@ -8,6 +9,7 @@ export const card = pgTable('card', {
     state_id: uuid('state_id').references(() => state.id, { onDelete: 'cascade', onUpdate: 'cascade' }).notNull(),
     title: varchar('title', { length: 255 }).notNull(),
     text: varchar('text', { length: 255 }).notNull(),
+    user_card: varchar('user_card').references(() => user.username, {onDelete: 'set null', onUpdate: 'cascade'}),
 });
 
 export type Card = typeof card.$inferSelect;
